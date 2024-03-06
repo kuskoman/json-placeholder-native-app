7fa9977f154715ed009a58da85a8207698efd664
Jakub <kubasurdej@gmail.com>
2024-03-06 21:44:29 +0100

    Make register screen actually working

--- app/(tabs)/register.tsx
+++ app/(tabs)/register.tsx
@@ -1,11 +1,22 @@
 import { RegisterForm } from "@/components/Auth/RegisterForm/RegisterForm";
 import { UserCreateModel } from "@/models/userModels";
+import { RootStackParamList } from "@/routes";
+import { UsersService } from "@/services/usersService";
+import { setUser } from "@/store/userSlice";
+import { NavigationProp } from "@react-navigation/native";
+import { useNavigation } from "expo-router";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { useDispatch } from "react-redux";
 
 const RegisterScreen: React.FC = () => {
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const dispatch = useDispatch();
+
   const handleRegisterSubmit = (user: UserCreateModel) => {
-    console.log(user);
+    const userWithId = { ...user, id: 1 };
+    dispatch(setUser(userWithId));
+    navigation.navigate("index");
   };
 
   return (
